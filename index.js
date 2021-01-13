952dafbe95a823d5a9ff835c10640970ef5b14e3
DronAce <okeynazone@gmail.com>
2021-01-13 13:28:30 -0500

    commit tree ffe28f5603ef5ff0ecea278a6de5163787398033

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array) {
+    const cards = [];
+    for (var i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number)
+        number -= 1
+    }
+}
